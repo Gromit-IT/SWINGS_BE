d127a01d6bf6ae26d7a77b9e6cd979fc3a35e9cc
Gromit-IT <165180593+Gromit-IT@users.noreply.github.com>
2025-03-04 15:48:28 +0900

    기본 세팅

---
+++ swings_be/src/main/java/com/swings/SwingsBeApplication.java
@@ -0,0 +1,13 @@
+package com.swings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwingsBeApplication.class, args);
+    }
+
+}
